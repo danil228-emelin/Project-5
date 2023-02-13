8571ea4cf5b4b8e20c10eddb7ba4b57bfa4438bf
danil emelin <danil228aeedj@mail.ru>
2023-02-13 21:27:36 +0300

    add    RemoveGreater

---
+++ src/main/java/itmo/p3108/command/RemoveGreater.java
@@ -0,0 +1,67 @@
+package itmo.p3108.command;
+
+import itmo.p3108.command.type.IndependentCommand;
+import itmo.p3108.command.type.NoArgumentCommand;
+import itmo.p3108.model.Person;
+import itmo.p3108.model.PersonReadingBuilder;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class RemoveGreater implements NoArgumentCommand, IndependentCommand {
+    private static RemoveGreater removeGreater;
+    @Setter
+    @NonNull
+    private Comparator<Person> comparator = Comparator.comparing(Person::getName).thenComparing(Person::getBirthday);
+
+    private RemoveGreater() {
+    }
+
+    public static RemoveGreater getInstance() {
+        if (removeGreater == null) {
+            removeGreater = new RemoveGreater();
+        }
+        return removeGreater;
+    }
+
+    @Override
+    public String execute() {
+
+        PersonReadingBuilder personReadingBuilder = PersonReadingBuilder.getInstance();
+        Person person = Person
+                .builder()
+                .name(personReadingBuilder.createName())
+                .id(personReadingBuilder.createId())
+                .height(personReadingBuilder.createHight())
+                .eyeColor(personReadingBuilder.createColor())
+                .nationality(personReadingBuilder.createNationality())
+                .birthday(personReadingBuilder.createBirthDay())
+                .coordinates(personReadingBuilder.createCoordinates())
+                .creationDate(ZonedDateTime.now())
+                .location(personReadingBuilder.createLocation())
+                .build();
+        ArrayList<Person> arrayList = controller.getPersonList();
+        int size = arrayList.size();
+        arrayList.removeIf(x -> {
+                    if (comparator.compare(x, person) > 0) {
+                        System.out.println(x.getName() + " удален ");
+                        return true;
+                    }
+                    return false;
+                }
+        );
+        if (size != arrayList.size()) {
+            return "Все подходящие элементы успешно удалены";
+        }
+        return "Ни один элемент не был удален";
+    }
+
+    @Override
+    public String name() {
+        return "remove_greater";
+    }
+
+}
